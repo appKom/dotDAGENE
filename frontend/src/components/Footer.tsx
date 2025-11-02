595daa8169f27f91b15fad5631369e866ee7d06d
Leonardo Lind-Milekvist <leonarli@stud.ntnu.no>
2025-11-02 16:52:59 +0100

    feat: :zap: Style; change footer logo and style

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import Logo from '../assets/dotDAGENEhovedlogo.svg';
+import Logo from '../assets/litenDfarget.svg';
 import Instagram from '../assets/instagram.svg';
 import Linkedin from '../assets/linkedin.svg';
 import Facebook from '../assets/facebook.svg';
@@ -10,7 +10,7 @@ export const Footer = () => {
     <footer className="mt-20 flex justify-center border-t-3 bg-black">
       <section className="m-8 mt-12 flex w-full flex-col justify-around gap-2 md:flex-row md:flex-wrap md:gap-8">
         <section>
-          <img src={Logo} alt="Logo" className="w-1/3 md:mb-4" />
+          <img src={Logo} alt="Logo" className="w-16 md:mb-4" />
           <p className="mb-5 w-3/4 text-gray-100 md:w-4/5 md:text-xl">
             NTNUs nyeste karrieredag innen digitalisering og teknologi.
           </p>
